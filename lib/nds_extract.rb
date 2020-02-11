f911f476ffee4726f70a0f122b9f335f6fda997c
Nduka-Eze Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-11 16:52:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -21,11 +21,11 @@ gross_for_director(directors_database[0])
 def directors_totals(nds)	  
   result = {}
   director_index = 0
-  while director_index < nds.length do
-    current_director = nds[director_index][:name]
-    result[current_director] = gross_for_director( nds[director_index] )
-    director_index += 1
+  while director_index < nds.count do
+      director_name = nds[director_index][:name]
+      result[director_name] = gross_for_director(nds[director_index])
+      director_index += 1
   end
-  result
-end	
+  pp result
+end
 
